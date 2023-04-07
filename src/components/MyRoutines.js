81cf83550927d1b2a2cdb54c74badf8245da5635
Matthew Rafferty <mattraffert@gmail.com>
2023-04-07 16:06:36 -0400

    Attach Activities work but have to double click

--- src/components/MyRoutines.js
+++ src/components/MyRoutines.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { fetchAllPosts } from './Fetch'
-import { renderAllPosts, renderNewPostForm, renderLogin, renderRegister } from './Render'
+import { Activities, renderNewPostForm, renderLogin, renderRegister } from './'
 
 const fetchAllRoutines = async ( username, token ) => {
     try {
@@ -20,19 +20,80 @@ const fetchAllRoutines = async ( username, token ) => {
       }
 };
 
+const fetchAllActivities = async () => {
+  try {
+      const response = await fetch(`https://fitness-tracker-gsjx.onrender.com/api/activities`);
+      const result = await response.json();
+      if (result.error) {
+          throw result.error;
+      }
+      console.log("Attached completed", result)
+      return result;
+    } catch (error) {
+      console.error('Error fetching things', error);
+    }
+};
 
-const MyRoutines = ({ token, user }) => {
+const attachActivity = async ( routineId, obj ) => {
+  try {
+      const response = await fetch(`https://fitness-tracker-gsjx.onrender.com/api/routines/${routineId}/activities`, {
+        method: "POST",
+        headers: {
+        'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(obj)
+      })
+      const result = await response.json();
+      if (result.error) {
+          throw result.error;
+      }
+      return result;
+    } catch (error) {
+      console.error('Error fetching things', error);
+    }
+};
+
+
+const MyRoutines = ({ token, setToken, user }) => {
     const [routines, setRoutines] = useState([])
+    const [open, setOpen] = useState(false);
+    const [activities, setActivities] = useState([])
+    const [activityId, setactivityId] = useState()
+    const [routineId, setroutineId] = useState()
+    const [count, setcount] = useState()
+    const [duration, setduration] = useState()
 
     useEffect(() => {
         const fetchData = async () => {
           const result = await fetchAllRoutines(user, token);
           setRoutines(result);
-
+          const actResult = await fetchAllActivities();
+          setActivities(actResult);
         };
     
         fetchData();
       }, []);
+
+      function handleOpen() {
+        setOpen(!open);
+      }
+
+      const handleAttach = async event => {
+        event.preventDefault();
+
+        console.log("NNNNNNNNNNNNNNNNNN", event.target[4].value)
+
+        setactivityId(event.target[3].value)
+        setroutineId(event.target[4].value)
+
+        console.log("ZZZZZZZZZZZZZZZZ", activityId, routineId, count, duration)
+
+        const obj = {
+          activityId, count , duration
+        }
+          await attachActivity(routineId, obj);
+      }
+
     return routines.map((routine) => (
         <div key={routine.id} class="routine">
             <div>
@@ -42,6 +103,32 @@ const MyRoutines = ({ token, user }) => {
                 <div>Creator Name: {routine.creatorName}</div> 
                 <div>Public: {routine.isPublic ? "Yes" : "No"}</div> 
                 <div>Goal: {routine.goal}</div> 
+                <div class="dropdown">
+                  Attach Activities
+                  <button onClick={handleOpen} class="dropbtn">Dropdown</button>
+                  <div>
+                    {open ? activities.map((activity) => (
+                      <div key={activity.id} class="routine">
+                          <form onSubmit={handleAttach}>
+                              <button type="submit">Double Click To Attach Activity</button>
+                              <h5>{activity.name}</h5>
+                              <label>
+                                <p>Count</p>
+                                <input type="number" onChange={event => setcount(event.target.value)} placeholder="Count..."/>
+                              </label>
+                              <label>
+                                <p>Duration</p>
+                                <input type="number" onChange={event => setduration(event.target.value)} placeholder="Duration..."/>
+                              </label>
+                              <div>id: {activity.id}</div> 
+                              <div>Description: {activity.description}</div> 
+                              <input value={activity.id} class="hidden"></input>
+                              <input value={routine.id} class="hidden"></input>
+                          </form> 
+                      </div>
+                    )) : null}
+                  </div>
+                </div>
                 <div>Activities: {routine.activities.map((activity) => (
                     <div key={activity.id} class="inner">
                         <div>Id: {activity.id}</div> 
